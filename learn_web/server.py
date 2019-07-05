298950f6caf18001fef4301fdc5a4de26d4e4c5a
Dmitrii Grishin <dmigrishin@gmail.com>
2019-07-05 17:30:14 +1000

    added dockerization of app

--- learn_web/server.py
+++ learn_web/server.py
@@ -11,4 +11,4 @@ def index():
     return render_template("index.html", page_title = title, weather = weather, news_list = news_list)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port=5000,debug=True,use_reloader=True)
